bf675d66d8ac761485e301f3b7ccd869b4084f91
Carlos Cassin Junior <c.cassinjunior@gmail.com>
2026-04-12 20:16:05 +0100

    Add personal data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-console.log("Hello" as string);
-
 type Timestamp = number;
 type Grams = number;
 type WeightEntry = { weight: Grams; timestamp: Timestamp };
@@ -14,9 +12,20 @@ type DateT = { day: number; month: number; year: number };
 type Centimeters = number;
 
 type PhysicalInfo = {
+  name: string;
   height: Centimeters;
   sex: "male" | "female" | "other";
   birthdate: DateT;
   fitness_level: FitnessLevel;
   weight: Array<WeightEntry>;
 };
+
+const ccassin: PhysicalInfo = {
+  name: "Carlos Junior",
+  height: 181,
+  sex: "male",
+  birthdate: { day: 26, month: 7, year: 1994 },
+  fitness_level: { type: "FitnessCategory", category: "moderately active" },
+  weight: [{ weight: 77000, timestamp: 1776016587021 }],
+};
+console.log(ccassin);
