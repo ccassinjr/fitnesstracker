bd8d115a50ec860c53c8cf197917df3b52bbb4f5
Carlos Cassin Junior <c.cassinjunior@gmail.com>
2026-04-18 22:12:17 +0100

    Show weight history

--- src/index.ts
+++ src/index.ts
@@ -55,11 +55,32 @@ function handleUpdatePhysicalInfo(e: SubmitEvent) {
   render(database, document.body);
 }
 
+function showTimestamp(timestamp: Timestamp): string {
+  return new Date(timestamp).toString();
+}
+
+function showWeight(grams: Grams): string {
+  if (grams < 1000) {
+    return grams + " g";
+  }
+  return grams / 1000 + " kg";
+}
+
 // This function takes a Datbase and an HTML element, and
 // displays the database's content inside the given HTML element.
 function render(db: Database, element: HTMLElement) {
   const weight = db.physicalInfo.weight;
   const lastWeight = weight[weight.length - 1];
+
+  function weightEntryToRow(entry: WeightEntry): string {
+    return `
+      <tr>
+      <td>${showWeight(entry.weight)}</td>
+      <td>${showTimestamp(entry.timestamp)}</td>
+      </tr>
+      `;
+  }
+
   element.innerHTML = `
   <h2>Physical Info</h2>
   <p>Name: ${db.physicalInfo.name}
@@ -68,12 +89,23 @@ function render(db: Database, element: HTMLElement) {
   <p>Birthdate: ${db.physicalInfo.birthdate.day}/${db.physicalInfo.birthdate.month}/${db.physicalInfo.birthdate.year}</p>
   <p>Fitness level: ${db.physicalInfo.fitness_level.type === "FitnessCategory" ? db.physicalInfo.fitness_level.category : db.physicalInfo.fitness_level.daily_expenditure_calories + " kcal/day"}</p>
   <p>Latest weight: ${lastWeight !== undefined ? lastWeight.weight / 1000 + " kg" : "N/A"}</p>
+  <table>
+    <thead>
+      <tr>
+        <td>Weight</td>
+        <td>Date</td>
+      </tr>
+    </thead>
+    <tbody>
+      ${db.physicalInfo.weight.map(weightEntryToRow).join("\n")}
+    </tbody>
+  </table>
   <hr/>
  
   <h2>Edit Physical info</h2>
   <form onsubmit="handleUpdatePhysicalInfo(event)">
     <h3>Add new weight measurement</h3>
-    <input name="weight" type="number" placeholder="Weight in grams"/>
+    <input name="weight" type="number" placeholder="Weight in grams" required/>
     <button type="submit">Add weight measurement</button>
   </form>
   `;
